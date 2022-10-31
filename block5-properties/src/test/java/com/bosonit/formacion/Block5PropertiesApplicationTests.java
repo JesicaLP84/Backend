76e21244ddde5054fb39ed01ecd04187dcdadfc1
Jesica López <jesica.lopez@bosonit.com>
2022-10-31 19:02:50 +0100

    Properties and YML

---
+++ block5-properties/src/test/java/com/bosonit/formacion/Block5PropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bosonit.formacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Block5PropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
